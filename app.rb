ee49ddf4c082e19232e4fd6c01e75c3349a08a26
Anna Cotugno <annaeuridice@gmail.com>
2019-11-25 17:08:34 +0000

    viewing bookmarks

--- app.rb
+++ app.rb
@@ -6,6 +6,17 @@ class BookmarkManager < Sinatra::Base
   'Bookmark Manager'
   end
 
+  get '/bookmarks'do
+  @bookmarks = [
+          "http://www.makersacademy.com",
+          "http://www.destroyallsoftware.com",
+          "http://www.google.com"
+         ]
+
+  erb(:bookmarks)
+
+  end
+
 run! if app_file == $0
 
 end
